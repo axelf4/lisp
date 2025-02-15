8e2fce80b6b04c69ae18ce0a7fd544f5fccb3ee6
Axel Forsman <axel@axelf.se>
2025-02-15 09:24:21 +0100

    Fix out-of-bounds access in fxhash_str

--- src/fxhash.h
+++ src/fxhash.h
@@ -51,10 +51,10 @@ static inline uint64_t fxhash_finish(uint64_t s) {
 }
 
 static inline uint64_t fxhash_mul_mix(uint64_t x, uint64_t y) {
-	auto full = (unsigned _BitInt(128)) x * y;
+	auto z = (unsigned _BitInt(128)) x * y;
 	// The middle bits (top bits of low, bottom bits of high half)
 	// fluctuate the most on small input changes: XOR the halves.
-	return full ^ full >> 64;
+	return z ^ z >> 64;
 }
 
 static inline uint32_t fxhash_load32(const char p[static 4]) {
@@ -73,15 +73,17 @@ static inline uint64_t fxhash_str(size_t n, const char p[static n]) {
 	uint64_t s0 = 0x243f6a8885a308d3, s1 = 0x13198a2e03707344, // Digits of pi
 		prevent_trivial_zero_collapse = 0xa4093822299f31d0;
 	if (n > 16) { // Handle bulk
-		for (size_t off = 0; off < n - 16; off += 16) {
+		const char *end = p + n - 16;
+		do {
 			// Replace s1 with mix of s0, x and y, and s0 with s1,
 			// ensuring loop is unrollable to 2 independent streams.
-			uint64_t x = fxhash_load64(p + off), y = fxhash_load64(p + off + 8),
+			uint64_t x = fxhash_load64(p), y = fxhash_load64(p + 8),
 				t = fxhash_mul_mix(s0 ^ x, prevent_trivial_zero_collapse ^ y);
 			s0 = s1;
 			s1 = t;
-		}
-		p += n - 16;
+		} while ((p += 16) < end);
+		p = end;
+		n = 16;
 		goto do_16;
 	} else if (n >= 8) { do_16: s0 ^= fxhash_load64(p); s1 ^= fxhash_load64(p + n - 8); }
 	else if (n >= 4) { s0 ^= fxhash_load32(p); s1 ^= fxhash_load32(p + n - 4); }
