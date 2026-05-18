2f1d064a55293c885a43d16143b871630e5f8368
Axel Forsman <axel@axelf.se>
2026-05-18 22:27:54 +0200

    Load standard library

--- src/main.c
+++ src/main.c
@@ -12,6 +12,33 @@ static struct LispCtx *ctx;
 	// signal to be re-delivered.
 }
 
+static LispObject lisp_load(struct LispCtx *ctx, const char *filename) {
+	FILE *f;
+	if (!(f = fopen(filename, "r"))) die("Failed to open file");
+	fseek(f, 0, SEEK_END);
+	size_t length = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	char *buf;
+	if (!(buf = malloc(length + 1))) die("malloc failed");
+	if (fread(buf, 1, length, f) != length) die("fread failed");
+	fclose(f);
+	buf[length] = '\0';
+
+	const char *s = buf;
+	LispObject form, result = NIL(ctx);
+	for (;;) {
+		enum LispReadError err = lisp_read(ctx, &s, &form);
+		switch (err) {
+		case LISP_READ_OK: result = lisp_eval(ctx, form); break;
+		case LISP_READ_EMPTY:
+			if (*s != '\0') { err = LISP_READ_EOF; goto err; }
+			free(buf);
+			return result;
+		default: err: die("Error: %d", err);
+		}
+	}
+}
+
 int main() {
 	if (!(ctx = lisp_new())) return EXIT_FAILURE;
 
@@ -20,6 +47,8 @@ int main() {
 	sigemptyset(&action.sa_mask);
 	action.sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESTART;
 	if (sigaction(SIGSEGV, &action, NULL)) return EXIT_FAILURE;
+
+	lisp_load(ctx, "stdlib.lisp");
 
 	char line[256];
 	while (fgets(line, sizeof line, stdin)) {
