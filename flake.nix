bf3cdedba642efde99015afcdb77d2b97362b8d2
Axel Forsman <axel@axelf.se>
2026-08-09 11:39:44 +0200

    Add manpage

--- flake.nix
+++ flake.nix
@@ -13,11 +13,19 @@
       strictDeps = true;
       nativeBuildInputs = [ pkgs.cmake ];
       checkInputs = [ pkgs.cmocka ];
+      outputs = [ "out" "man" ];
 
       doCheck = true;
 
       hardeningDisable = [ "all" ];
       env.CFLAGS = "-march=x86-64-v3 -fomit-frame-pointer";
+
+      meta = {
+        description = "Tracing JIT Lisp compiler";
+        homepage = "https://github.com/axelf4/lisp";
+        license = pkgs.lib.licenses.gpl3Plus;
+        platforms = pkgs.lib.platforms.all;
+      };
     };
 
     devShells.${system}.default = pkgs.mkShell.override { inherit stdenv; } {
