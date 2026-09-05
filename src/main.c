2c0fc4bb90be6c6a8e51d821d2042ae3492b0163
Axel Forsman <axel@axelf.se>
2026-09-05 16:01:59 +0200

    Parse command-line arguments

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <errno.h>
+#include <getopt.h>
 #include <signal.h>
 #include <unistd.h>
 #include "lisp.h"
@@ -10,7 +11,31 @@
 
 struct Str { size_t len; const char *p; };
 
+struct Config {
+	const char *script; ///< File name of script to run, or NULL.
+};
+
 static struct LispCtx *ctx;
+
+static bool parse_args(int argc, char *argv[], struct Config *config) {
+	const char *shortopts = "s:";
+	struct option longopts[] = {
+		{"script",     required_argument, NULL,  's' },
+		{}
+	};
+	int c;
+
+	while ((c = getopt_long(argc, argv, shortopts, longopts, NULL)) >= 0)
+		switch (c) {
+		case 's': config->script = optarg; break;
+		case '?': goto err;
+		default: unreachable();
+		}
+
+	if (optind >= argc) return true;
+err: fprintf(stderr, "usage: %s [--script=<file>]\n", *argv);
+	return false;
+}
 
 [[gnu::cold]] static void signal_handler(int sig, siginfo_t *info, void *ucontext) {
 	if (lisp_signal_handler(sig, info, ucontext, ctx)) return;
@@ -32,7 +57,7 @@ static size_t search_up(size_t len, char s[static len], struct Str *landmarks) {
 	return 0;
 }
 
-static void init_lisp_path(struct LispCtx *ctx, const char *argv0) {
+static void init_lisp_path(const struct Config *config, const char *argv0) {
 	LispObject list = NIL(ctx);
 
 	if (!argv0) argv0 = "";
@@ -50,8 +75,12 @@ static void init_lisp_path(struct LispCtx *ctx, const char *argv0) {
 
 	// TODO Consult LISPPATH environment variable
 
-	// Push directory of input script or the current directory
-	list = cons(ctx, lisp_str(ctx, 0, ""), list);
+	list = cons(ctx, lisp_str(ctx, 0, ""), list); // Push the current directory
+
+	// Push directory of input script
+	const char *script = config->script, *script_base;
+	if (script && (script_base = strrchr(script, '/')))
+		list = cons(ctx, lisp_str(ctx, script_base - script, script), list);
 
 	struct LispSymbol *sload_path = UNTAG_OBJ(LISP_INTERN(ctx, "load-path"));
 	sload_path->value = list;
@@ -59,6 +88,7 @@ static void init_lisp_path(struct LispCtx *ctx, const char *argv0) {
 
 /** Executes a file of Lisp code named @p filename. */
 static LispObject lisp_load(struct LispCtx *ctx, const char *filename) {
+	// TODO Support reading from - or /dev/stdin
 	size_t filename_len = strlen(filename);
 	char *buf = NULL;
 	FILE *f;
@@ -101,7 +131,25 @@ out_free_buf: free(buf);
 	if (ok) return result; else throw(1);
 }
 
-int main([[maybe_unused]] int argc, char *argv[]) {
+void do_repl() {
+	puts("Type forms to execute:");
+	char line[256];
+	while (fputs("> ", stdout), fgets(line, sizeof line, stdin)) {
+		LispObject form;
+		enum LispReadError error;
+		if ((error = lisp_read_whole(ctx, line, &form)))
+			fprintf(stderr, "lisp_read failed: %d\n", error);
+		else {
+			lisp_print(ctx, lisp_eval(ctx, form), stdout);
+			putchar('\n');
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	struct Config config;
+	if (!parse_args(argc, argv, &config)) return EXIT_FAILURE;
+
 	if (!(ctx = lisp_new())) return EXIT_FAILURE;
 
 	struct sigaction action;
@@ -110,20 +158,10 @@ int main([[maybe_unused]] int argc, char *argv[]) {
 	action.sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESTART;
 	if (sigaction(SIGSEGV, &action, NULL)) return EXIT_FAILURE;
 
-	init_lisp_path(ctx, *argv);
+	init_lisp_path(&config, *argv);
 	lisp_load(ctx, "core.lisp");
 
-	char line[256];
-	while (fgets(line, sizeof line, stdin)) {
-		LispObject form;
-		enum LispReadError error;
-		if ((error = lisp_read_whole(ctx, line, &form)))
-			fprintf(stderr, "Error: %d\n", error);
-		else {
-			lisp_print(ctx, lisp_eval(ctx, form), stdout);
-			putchar('\n');
-		}
-	}
+	if (config.script) lisp_load(ctx, config.script); else do_repl();
 
 #ifndef NDEBUG
 	lisp_free(ctx);
